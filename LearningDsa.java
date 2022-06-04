5ed16e17f618339a1f470a17e614a440a8c5d15c
Jitesh Singh <jiteshs101@gmail.com>
2022-06-04 21:14:18 +0530

    SinglyLinkedList Node Creation

--- LearningDsa.java
+++ LearningDsa.java
@@ -1,6 +1,31 @@
+import linkedlist.Node;
+
 public class LearningDsa {
 
     public static void main(String[] args) {
-        System.out.println(" Basic BoilerPlate Code");
+
+        Node sllHead = printSLLCreateNode();
+
+    }
+
+    static Node printSLLCreateNode() {
+
+        // Node Creation
+        Node sllNode1 = new Node(10);
+        Node sllNode2 = new Node(20);
+        Node sllNode3 = new Node(30);
+        Node sllNode4 = new Node(40);
+
+        // Linking Nodes
+        Node head = sllNode1;
+        head.nxt = sllNode2;
+        sllNode2.nxt = sllNode3;
+        sllNode3.nxt = sllNode4;
+        sllNode4 = null; // tail
+
+        System.out.println("SLL Created Successfully");
+
+        return head;
+
     }
 }
