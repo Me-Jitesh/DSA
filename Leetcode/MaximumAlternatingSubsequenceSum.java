c65c264b0402c26069d6f6d5a3b5ffa3ea2cadd6
Jitesh Singh <Jiteshs101@gmail.com>
2023-08-13 17:38:30 +0530

    Leetcode :  1911. Maximum Alternating Subsequence Sum Bottom Up

--- Leetcode/MaximumAlternatingSubsequenceSum.java
+++ Leetcode/MaximumAlternatingSubsequenceSum.java
@@ -2,19 +2,19 @@ package Leetcode;
 
 // 1911. Maximum Alternating Subsequence Sum
 
-import java.util.Arrays;
-
 public class MaximumAlternatingSubsequenceSum {
 
     public long maxAlternatingSum(int[] nums) {
 
 //        return solveRecursion(nums, 0, 1);
 
-        long[][] dp = new long[nums.length + 1][2];
-        for (long[] x : dp) {
-            Arrays.fill(x, -1);
-        }
-        return solveRecursionMemoi(nums, 0, 1, dp);
+//        long[][] dp = new long[nums.length + 1][2];
+//        for (long[] x : dp) {
+//            Arrays.fill(x, -1);
+//        }
+//        return solveRecursionMemoi(nums, 0, 1, dp);
+
+        return solveBottomUp(nums);
     }
 
     private long solveRecursion(int[] nums, int idx, int isEven) {
@@ -55,5 +55,28 @@ public class MaximumAlternatingSubsequenceSum {
         long skip = solveRecursionMemoi(nums, idx + 1, isEven, dp);
 
         return dp[idx][isEven] = Math.max(take, skip);
+    }
+
+    private long solveBottomUp(int[] nums) {
+
+        long[][] t = new long[nums.length + 1][2];
+        t[0][1] = 0;
+        t[0][0] = 0;
+
+        for (int i = 1; i <= nums.length; i++) { // Even = 1 , Odd = 0
+            // Become Even after consider ith value
+            long considerToEve = t[i - 1][0] - nums[i - 1];    // nums[i] subtract from odds
+            long ignoreToEve = t[i - 1][1];                // return prev even max val
+
+            t[i][1] = Math.max(considerToEve, ignoreToEve); // Store at even array
+
+            // Become Odd after consider ith value
+            long considerToOdd = t[i - 1][1] + nums[i - 1];    // nums[i] added to odds
+            long ignoreToOdd = t[i - 1][0];                // return prev odd max val
+
+            t[i][0] = Math.max(considerToOdd, ignoreToOdd);  // Store at even array
+        }
+
+        return Math.max(t[nums.length][1], t[nums.length][0]);
     }
 }
